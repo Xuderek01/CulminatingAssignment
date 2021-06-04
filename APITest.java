f86d9eefba59509b47b9588835aad46400073be5
Xuderek01 <epnightmare02@gmail.com>
2021-06-04 17:50:14 -0400

    Update APITest.java

--- APITest.java
+++ APITest.java
@@ -11,89 +11,117 @@ class APITest extends Frame implements ActionListener, WindowListener{ //Class t
         menu(); //Calls on menu method
     }
 
+    public static void graphs(){
+        JFrame frame2 = new JFrame("Physics Application - Options");
+        
+
+}
+    
+
     public static void formulas(){ //Formula screen method
         JFrame frame1 = new JFrame("Physics Application - Options"); //Initialize all variables and objects
-        JLabel formulaTitle;
+        JLabel formulaTitle, inputValue, equation, chosenEq, answer;
         Choice c;
-        JButton confirm;
-        JButton calculate;
-        String option1 = "Item1";
-        String option2 = "Item2";
-        JTextField var1, var2;
+        JButton confirm, back;
+        JTextField acceleration, iVelo, fVelo, displace, force, mass, time, velocity, height;
+        String option1 = "Acceleration Formula";
+        String option2 = "Displacement Formula";
+        String option3 = "Force Formula";
+        String option4 = "Free Fall Formula";
+        String option5 = "Velocity Formula";
+        String option6 = "Work Formula";
+        String option7 = "Power Formula";
+        String option8 = "Potential Gravitational Energy Formula";
+        String option9 = "Potential Kinetic Energy Formula";
 
-        var1 = new JTextField(); //Initialize textField and set boundaries
-        var1.setBounds(0,0,0,0);
+        answer = new JLabel("ANSWER: ", JLabel.CENTER);
+        answer.setBounds(100, 600, 800, 100);
 
-        var2 = new JTextField();
-        var2.setBounds(0,0,0,0);
-
-        calculate = new JButton("Calculate"); //Create new button
-
-        JLabel inputValue = new JLabel("", JLabel.CENTER); //Create new label and set boundaries
+        chosenEq = new JLabel("", JLabel.CENTER);
+        chosenEq.setBounds(250, 290, 500, 75);
+        
+        inputValue = new JLabel("", JLabel.CENTER); //Create new label and set boundaries
         inputValue.setBounds(100, 290, 800, 100);
 
-        JLabel equation = new JLabel("", JLabel.CENTER);
+        equation = new JLabel("", JLabel.CENTER);
         equation.setBounds(250, 240, 500, 100);
 
         formulaTitle = new JLabel("Formulas", JLabel.CENTER);
         formulaTitle.setBounds(250, 50, 500, 75);
         
         c = new Choice(); //Set dropbox, set boundaries, and add options
-        c.setBounds(450, 170, 100, 30);
+        c.setBounds(400, 170, 200, 30);
         c.add(option1);
         c.add(option2);
+        c.add(option3);
+        c.add(option4);
+        c.add(option5);
+        c.add(option6);
+        c.add(option7);
+        c.add(option8);
+        c.add(option9);
 
         confirm = new JButton("confirm"); //Set button and determine what happens when clicked
         confirm.setBounds(450, 205, 100, 30);
         confirm.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent f) {
-                String data = "Option Selected: " + c.getItem(c.getSelectedIndex()); //pulls data from option selected
+                String data = c.getItem(c.getSelectedIndex()); //pulls data from option selected
                 equation.setText(data);//Sets label to data variable
-                
-                
-                
-                if (c.getItem(c.getSelectedIndex()).equals(option1)){ //if option is selected
-                    var1.setBounds(50, 490, 100, 30); //Set bounds for textFields
-                    var2.setBounds(50, 525, 100, 30);
 
-                    String var1String = var1.getText(); //Get text from textfields
-                    double varA = Integer.parseInt(var1String); //Turn text to integer
-
-                    String var2String = var1.getText();
-                    double varB = Integer.parseInt(var2String);
-
-
-
-                    inputValue.setText("a + b");
-
-                    
-                    calculate.setBounds(450, 375, 100, 30);
-                    calculate.addActionListener(new ActionListener(){
-                        public void actionPerformed(ActionEvent g){
-                            int ans = varA + varB; //Calculate value
-
-                        }
-                    });
+                if (c.getItem(c.getSelectedIndex()).equals(option1)){
+                    chosenEq.setText("(Vf - Vi)/t");
                 }
                 else if (c.getItem(c.getSelectedIndex()).equals(option2)){
-                    inputValue.setText("b + a");
+                    chosenEq.setText("Vf - Vi");
                 }
-                
+                else if (c.getItem(c.getSelectedIndex()).equals(option3)){
+                    chosenEq.setText("m x a");
+                }
+                else if (c.getItem(c.getSelectedIndex()).equals(option4)){
+                    chosenEq.setText("(1/2 x 9.8) x t^2");
+                }
+                else if (c.getItem(c.getSelectedIndex()).equals(option5)){
+                    chosenEq.setText("d/t");
+                }
+                else if (c.getItem(c.getSelectedIndex()).equals(option6)){
+                    chosenEq.setText("f x d");
+                }
+                else if (c.getItem(c.getSelectedIndex()).equals(option7)){
+                    chosenEq.setText("m x g x h");
+                }
+                else if (c.getItem(c.getSelectedIndex()).equals(option8)){
+                    chosenEq.setText("1/2m x v^2");
+                }
+                else if (c.getItem(c.getSelectedIndex()).equals(option9)){
+                    chosenEq.setText("1/2k x d^2");
+                }
 
-                
             }
         });
+
+        back = new JButton("Back");
+        back.setBounds(450, 800, 100, 30);
+        back.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent h){
+                menu();
+                frame1.setVisible(false);
+            }
+
+        });
+
         frame1.add(formulaTitle);
         frame1.add(c);
         frame1.add(confirm);
         frame1.add(equation);
         frame1.add(inputValue);
-        frame1.add(calculate);
-        frame1.add(var1);
-        frame1.add(var2);
+        frame1.add(chosenEq);
+        frame1.add(answer);
+        frame1.add(back);
         formulaTitle.setFont(new Font("Courier", Font.BOLD, 75));
         equation.setFont(new Font("Courier", Font.PLAIN, 35));
         inputValue.setFont(new Font("Courier", Font.PLAIN, 30));
+        chosenEq.setFont(new Font("Courier", Font.PLAIN, 30));
+        answer.setFont(new Font("Courier", Font.BOLD, 30));
         frame1.setSize(1000, 1000);
         frame1.setLayout(null);
         frame1.setVisible(true);
@@ -102,20 +130,25 @@ class APITest extends Frame implements ActionListener, WindowListener{ //Class t
     public static void menu(){
         JFrame frame = new JFrame("Physics Application"); //Names the frame
         JLabel title; //Creates new label(s)
-        JButton enter; //Creates new button(s)
-        title = new JLabel("Physics Application"); //Names the label
-        title.setBounds(50, 50, 100, 30); //Sets size and location of the label
-        enter = new JButton("ENTER"); //Names the button
-        enter.setBounds(50, 100, 100, 30); //Sets size and location of the button
-        enter.addActionListener(new ActionListener(){ //Adds an action listener to the button
+        JButton formula, graph; //Creates new button(s)
+        title = new JLabel("Physics Application", JLabel.CENTER); //Names the label
+        title.setBounds(250, 295, 500, 75); //Sets size and location of the label
+        formula = new JButton("formula"); //Names the button
+        formula.setBounds(375, 375, 250, 75); //Sets size and location of the button
+        formula.addActionListener(new ActionListener(){ //Adds an action listener to the button
             public void actionPerformed(ActionEvent e){ //When the button is pressed, perform actions 
                 formulas(); //Calls on formulas method
-                frame.setVisible(false); //Makes menu non-interactable and invisible
+                frame.setVisible(false);
             }
         });
+
+        graph = new JButton("Graph");
+        graph.setBounds(375, 460, 250, 75);
         
         frame.add(title); //Add title to frame
-        frame.add(enter); //Add button to frame
+        frame.add(formula); //Add button to frame
+        frame.add(graph);
+        title.setFont(new Font("Courier", Font.BOLD, 50));
         frame.setSize(1000, 1000); //Set frame size
         frame.setLayout(null); //Set layout
         frame.setVisible(true); //Determine if frame is visible and interactable
